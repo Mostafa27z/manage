2dca1b502ffd1c9cf9af7fe751772277f4f4af71
Mostafa27z <123766386+Mostafa27z@users.noreply.github.com>
2023-04-05 15:26:08 +0200

    Add files via upload

---
+++ js.js
@@ -0,0 +1,76 @@
+let w = document.documentElement.clientWidth;
+let cr = document.querySelectorAll(".cr");
+let spn = document.querySelectorAll(".spn");
+if(w<1240){
+	$(".spns").show();
+	for (var i = 1; i < cr.length; i++) {
+		$(cr[i]).hide();
+	}
+	$(spn[0]).css("background-color","hsl(12, 88%, 59%)");
+}
+else{
+	$(".spns").hide();
+	for (var i = 1; i < cr.length; i++) {
+		$(cr[i]).show();
+	}
+	
+}
+spn.forEach((e,j) =>{
+	e.addEventListener("click", ()=>{
+		for (var i = 0; i < cr.length; i++) {
+		$(cr[i]).hide();
+		}
+		for (var i = 0; i < cr.length; i++) {
+		$(spn[i]).css("background-color","white");
+		}
+		$(cr[j]).show();
+		$(spn[j]).css("background-color","hsl(12, 88%, 59%)");
+	})
+})
+window.addEventListener("resize", ()=>{
+	w = document.documentElement.clientWidth;
+
+if(w<1240){
+	$(".spns").show();
+	for (var i = 1; i < cr.length; i++) {
+		$(cr[i]).hide();
+	}
+	$(spn[0]).css("background-color","hsl(12, 88%, 59%)");
+	spn.forEach((e,j) =>{
+	e.addEventListener("click", ()=>{
+		for (var i = 0; i < cr.length; i++) {
+		$(cr[i]).hide();
+		}
+		for (var i = 0; i < cr.length; i++) {
+		$(spn[i]).css("background-color","white");
+		}
+		$(cr[j]).show();
+		$(spn[j]).css("background-color","hsl(12, 88%, 59%)");
+	})
+})
+}
+else{
+	$(".spns").hide();
+	for (var i = 0; i < cr.length; i++) {
+		$(cr[i]).show();
+	}
+}
+
+});
+
+let form = document.getElementsByTagName('form');
+form[0].addEventListener('submit', (e)=>{
+	let em = document.getElementById('email');
+	e.preventDefault();
+	if(em.value.trim()){
+		$(".danger").css("opacity","0");
+		$('#email').css("outline-style","none");
+	}
+	else{
+		$(".danger").css("opacity","1");
+		$('#email').css("outline-style","solid");
+		$('#email').css("outline-width","1px");
+		$('#email').css("outline-color","red");
+	}
+	
+})
